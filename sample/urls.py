df211ef1adc1df718c5171ea70715dc79acafd01
leensungzero <brianwoo119@naver.com>
2018-08-13 01:50:25 +0900

    Ep01 Project Settings

---
+++ sample/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+
+app_name = "sample"
+
+
+router = DefaultRouter()
+router.register('posts', views.PostViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
