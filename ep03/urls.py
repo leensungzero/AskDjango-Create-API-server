e65f1103fff25a53503515b317216ea99f912f23
leensungzero <brianwoo119@naver.com>
2018-08-13 22:04:38 +0900

    [ep03] Post Detail/Update/Delete API 구현

--- ep03/urls.py
+++ ep03/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'ep03'
 
 urlpatterns = [
-    path('post/', views.PostListAPIView.as_view()),   
+    path('post/', views.PostListAPIView.as_view()),
+    path('post/<int:pk>/', views.PostDetailAPIView.as_view()),
 ]
