132e7d95ef2c300dde09a771d7281848678795e2
leensungzero <brianwoo119@naver.com>
2018-08-14 03:02:08 +0900

    PostViewSet을 Router를 통해 URLConf에 등록

--- ep04/urls.py
+++ ep04/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
 
 app_name = 'ep04'
 
+router = routers.DefaultRouter()
+router.register('post', views.PostViewSet) # 2개의 URL을 처리하는 뷰함수를 만들어서 등록
+
 urlpatterns = [
-    path('post/', views.post_list),
-    path('post/<int:pk>/', views.post_detail),
+    path('', include(router.urls)),
+    # path('post/', views.post_list),
+    # path('post/<int:pk>/', views.post_detail),
 ]
