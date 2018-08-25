65b572d21b7010dce6c236b14ce1a526c6664925
leensungzero <brianwoo119@naver.com>
2018-08-25 15:50:57 +0900

    APIView에서의 get_queryset 필터링

--- ep04/serializers.py
+++ ep04/serializers.py
@@ -5,4 +5,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'is_public', 'created_at', 'updated_at', ]
+        fields = ['id', 'title', 'content', 'is_public', 'created_at', 'updated_at', 'author', ]
--- ep06/views.py
+++ ep06/views.py
@@ -5,3 +5,11 @@ from ep04.serializers import PostSerializer
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.is_authenticated:
+            qs = qs.filter(author=self.request.user)
+        else:
+            qs = qs.none()
+        return qs
