58fc856bccfad3198d93660569a4558cef0b79f7
leensungzero <brianwoo119@naver.com>
2018-08-25 16:00:55 +0900

    SearchFilter를 통한 Post 필터링

--- ep06/views.py
+++ ep06/views.py
@@ -1,15 +1,25 @@
 from ep04.models import Post
 from rest_framework.viewsets import ModelViewSet
 from ep04.serializers import PostSerializer
+from rest_framework.filters import SearchFilter
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title']
 
+    '''
     def get_queryset(self):
         qs = super().get_queryset()
+
+        # search = self.request.GET.get('search', '')
+        # if search:
+        #     qs = qs.filter(title__icontains=search)
+
         if self.request.user.is_authenticated:
             qs = qs.filter(author=self.request.user)
         else:
             qs = qs.none()
         return qs
+    '''
